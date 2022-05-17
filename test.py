43fa5609a48b0d401ffa7b721e9315c60a006cca
kathukuttitest <105492332+kathukuttitest@users.noreply.github.com>
2022-05-17 17:19:52 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,35 +1,27 @@
-from email import message
 import ccxt
-import pandas_ta as ta
+# import pandas_ta as ta
 import pandas as pd
 import requests
-url = "https://api.telegram.org/bot5308554635:AAHXPltt3MUI3XRfLME-rQDYoRILrsmuqKs/sendMessage?chat_id=-1001595590550&parse_mode=HTML&"
+
+
+
+
+
+
 exchange = ccxt.binance()
 
-bars = exchange.fetch_ohlcv('ETH/USDT', timeframe='15m', limit=50)
-df = pd.DataFrame(bars, columns=['time', 'open', 'high', 'low', 'close', 'volume'])
 
-rsi = df.ta.rsi()
+data = exchange.fetch_ohlcv('BTCUSDT',"15m", limit=5)
+df = pd.DataFrame(data, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Volume'])
+df['Date']= pd.to_datetime(df['Date'], unit="ms")
 
-last_row = rsi.iloc[-1]
-
-
-
-if last_row >= 70:
-    alert = f"ETH is <b>overbought</b> - RSI value is {last_row}" 
-   
-    file = {
-        "text" : str(alert)
-    } 
-    requests.get(url, params=file)
-if last_row <= 30:
-    alert = f"ETH is <b>oversold</b> - RSI value is {last_row}"  
-   
-    file = {
-        "text" : str(alert)
-    }
-
-    requests.get(url, params=file)
-        
-
-
+last_row = df.iloc[-2]
+ 
+if last_row.Volume >= 2500:
+  img_url = "https://api.chart-img.com/v1/tradingview/advanced-chart?width=800&height=600&interval=15m&theme=light&key=5da40c6a-9cfb-4be4-8555-ca057139bdf4"
+  bot_url = "https://api.telegram.org/bot5308554635:AAHXPltt3MUI3XRfLME-rQDYoRILrsmuqKs/sendPhoto?chat_id=-1001595590550&parse_mode=HTML&"
+  file = {
+      'photo' : img_url,
+      'caption' : f"<b>BTC/USDT - Hight Volume Alert (15m)</b> \n\n Volume - {last_row.Volume} \n\n @cryptoguruakacg"
+  }
+  requests.post(bot_url,json=file)
