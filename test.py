dc8f76e9b0c60bbddd671ac41470f7fcd6826d61
kathukuttitest <105492332+kathukuttitest@users.noreply.github.com>
2022-05-23 11:50:56 +0530

    Update test.py

--- test.py
+++ test.py
@@ -15,8 +15,7 @@ while(True):
   last_row = df.iloc[-2]
   
   if last_row.Volume >= 2500:
-    img_url = "https://api.chart-img.com/v1/tradingview/advanced-chart?width=800&height=600&interval=15m&theme=light&key=5da40c6a-9cfb-4be4-8555-ca057139bdf4"
-    bot_url = "https://api.telegram.org/bot5308554635:AAHXPltt3MUI3XRfLME-rQDYoRILrsmuqKs/sendPhoto?chat_id=-1001595590550&parse_mode=HTML&"
+   
     file = {
         'photo' : img_url,
         'caption' : f"<b>BTC/USDT - Hight Volume Alert (15m)</b> \n\n Volume - {last_row.Volume} \n\n @cryptoguruakacg"
